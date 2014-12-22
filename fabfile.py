b53a19a4c3109bb8452a7cf67c46f1b7504939e6
Kévin Gomez <geek63@gmail.com>
2014-12-23 00:02:11 +0100

    Fix scaling method

--- fabfile.py
+++ fabfile.py
@@ -1,56 +1,98 @@
+from itertools import islice
+from time import sleep
 from fabric.api import task, local
+
+BOOT_WAIT_DELAY = 10 # seconds
+STOP_WAIT_DELAY = 5  # seconds
 
 @task
 def up():
     local('fig up -d')
 
+    # wait for the services in the containers to start
+    sleep(BOOT_WAIT_DELAY)
+
     # register web backend into hipache
     if hipache_has_frontend('test_app.loc'):
         unregister_all_backends('test_app.loc')
 
-    local('fig run rediscli rpush frontend:test_app.loc test_app')
+    rediscli('rpush frontend:test_app.loc test_app')
     backend_containers = get_container_ids('web')
     register_backend_containers('test_app.loc', backend_containers)
 
 @task
 def scale(service, number):
-    """
-    @todo: rewrite. Fig kills the containers before I can unregister them from
-    hipache
-    """
+    if service != 'web':
+        raise NotImplementedError('Only the "web" service can be scaled for now')
 
+    number = int(number)
     old_containers = get_container_ids(service)
-    # once containers are deleted, retrieving their IP inst' possible so we do
-    # it now
-    ips_map = {cid: get_container_ip(cid)  for cid in old_containers}
 
-    local('fig scale %s=%s' % (service, number))
+    # nothing to do
+    if len(old_containers) == number:
+        return
 
-    current_containers = get_container_ids(service)
+    if len(old_containers) < number:
+        start_new_backends(number - len(old_containers))
+    else:
+        stop_old_backends(len(old_containers) - number)
+
+
+def stop_old_backends(number, remove=False, wait=STOP_WAIT_DELAY):
+    # retrieve the container IDs to remove
+    containers_to_remove = take(number, get_container_ids('web'))
+
+    # unregister them from hipache
+    for cid in containers_to_remove:
+        unregister_backend('test_app.loc', get_container_ip(cid))
+
+    # wait a bit so that they have time to handle the ongoing requests
+    sleep(wait)
+
+    # and stop them
+    local('docker stop %s' % ' '.join(containers_to_remove))
+
+    # now we can safely remove them
+    if remove:
+        local('docker rm %s' % ' '.join(containers_to_remove))
+
+def start_new_backends(number):
+    old_containers = get_container_ids('web')
+
+    # create the new containers
+    local('fig scale web=%d' % (len(old_containers) + number))
+
+    # retrieve the created container IDs
+    current_containers = get_container_ids('web')
     added_containers = set(current_containers) - set(old_containers)
-    removed_containers = set(old_containers) - set(current_containers)
+
+    # wait for the services in the containers to start
+    sleep(BOOT_WAIT_DELAY)
 
     # register new backends
     register_backend_containers('test_app.loc', added_containers)
-
-    # unregister stopped backends
-    for cid in removed_containers:
-        unregister_backend('test_app.loc', ips_map[cid])
-
 
 def register_backend_containers(domain, containers_ids):
     for container_id in containers_ids:
         ip = get_container_ip(container_id)
         register_backend(domain, ip)
 
+def rediscli(command, redis_service_name='hipache', capture=False):
+    redis_cid = get_container_id(redis_service_name)
+
+    return local('docker exec %s redis-cli %s' % (redis_cid, command), capture=capture)
+
 def register_backend(domain, ip):
-    local('fig run --rm rediscli rpush frontend:%s http://%s:80' % (domain, ip))
+    rediscli('rpush frontend:%s http://%s:80' % (domain, ip))
 
 def unregister_backend(domain, ip):
-    local('fig run --rm rediscli lrem frontend:%s 0 http://%s:80' % (domain, ip))
+    rediscli('lrem frontend:%s 0 http://%s:80' % (domain, ip))
 
 def unregister_all_backends(domain):
-    local('fig run --rm rediscli ltrim frontend:%s 0 -1' % domain)
+    rediscli('ltrim frontend:%s 0 -1' % domain)
+
+def get_container_id(name):
+    return get_container_ids(name)[0]
 
 def get_container_ids(name):
     result = local('docker ps | grep %s | cut -d " " -f1' % name, capture=True)
@@ -63,6 +105,10 @@ def get_container_ip(container_id):
     return result.stdout
 
 def hipache_has_frontend(domain):
-    result = local('fig run --rm rediscli llen frontend:%s' % domain, capture=True)
+    result = rediscli('llen frontend:%s' % domain, capture=True)
 
     return result != "(integer) 0"
+
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(islice(iterable, n))
